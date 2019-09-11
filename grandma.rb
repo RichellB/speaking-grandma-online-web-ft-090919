a34c1c351e0ec7c047c2b74071ac026b47036cac
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-11 03:54:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.downcase
     p "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    p "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     p "I LOVE YOU TOO PUMPKIN!"
   else 
