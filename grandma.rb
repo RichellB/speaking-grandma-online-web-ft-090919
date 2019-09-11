38a7249451e3385e0e152c590abff4102948c750
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-11 03:58:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,8 @@ def speak_to_grandma(phrase)
     p "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     p "NO, NOT SINCE 1938!"
-  elsif phrase == 'I LOVE YOU GRANDMA!'
+  end
+  if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
   else 
     p "HUH?! SPEAK UP, SONNY!"
